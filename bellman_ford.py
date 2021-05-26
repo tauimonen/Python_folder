2ca45858bbf24b95d944f76ffc0cbb382f117662
Toni Uimonen <tauimonen@gmail.com>
2021-05-26 14:32:28 +0300

    Language modification

--- bellman_ford.py
+++ bellman_ford.py
@@ -1,43 +1,43 @@
-class Verkko:
-    def __init__(self, solmut):
-        self.solmut = solmut
-        self.verkko = []
+class Graph:
+    def __init__(self, node_count):
+        self.node_count = node_count
+        self.graph = []
 
-    def lisaa_kaari(self, u, v, w):
-        self.verkko.append([u, v, w])
+    def add_edge(self, u, v, w):
+        self.graph.append([u, v, w])
 
-    def tulosta(self, etaisyys):
-        print("Etäisyys")
-        for i in range(self.solmut):
-            print("{0}\t\t{1}".format(i, etaisyys[i]))
+    def tulosta(self, distance):
+        print("Distance")
+        for i in range(self.node_count):
+            print("{0}\t\t{1}".format(i, distance[i]))
 
-    def bf(self, lahto):
-        etaisyys = [float("Inf")] * self.solmut
-        etaisyys[lahto] = 0
+    def bf(self, start):
+        distance = [float("Inf")] * self.node_count
+        distance[start] = 0
 
-        for _ in range(self.solmut -1):
-            for u, v, w, in self.verkko:
-                if etaisyys[u] != float("Inf") and etaisyys[u] + w < etaisyys[v]:
-                    etaisyys[v] = etaisyys[u] + w
-                    print(v, "etäisyys: ", etaisyys[v])
-        for u, v, w in self.verkko:
-            if etaisyys[u] != float("Inf") and etaisyys[u] + w < etaisyys[v]:
-                print("negatiivinen sykli")
+        for _ in range(self.node_count - 1):
+            for u, v, w, in self.graph:
+                if distance[u] != float("Inf") and distance[u] + w < distance[v]:
+                    distance[v] = distance[u] + w
+                    print(v, "distance: ", distance[v])
+
+        for u, v, w in self.graph:
+            if distance[u] != float("Inf") and distance[u] + w < distance[v]:
+                print("Negative cycle")
                 return
-        self.tulosta(etaisyys)
+
+        self.tulosta(distance)
 
 
-verkko = Verkko(6)
-verkko.lisaa_kaari(0, 1, 10)
-verkko.lisaa_kaari(0, 3, 2)
-verkko.lisaa_kaari(1, 2, 10)
-verkko.lisaa_kaari(1, 4, 2)
-verkko.lisaa_kaari(2, 5, 2)
-verkko.lisaa_kaari(3, 1, 3)
-verkko.lisaa_kaari(3, 4, 8)
-verkko.lisaa_kaari(4, 2, 3)
-verkko.lisaa_kaari(4, 5, 8)
+graph = Graph(6)
+graph.add_edge(0, 1, 10)
+graph.add_edge(0, 3, 2)
+graph.add_edge(1, 2, 10)
+graph.add_edge(1, 4, 2)
+graph.add_edge(2, 5, 2)
+graph.add_edge(3, 1, 3)
+graph.add_edge(3, 4, 8)
+graph.add_edge(4, 2, 3)
+graph.add_edge(4, 5, 8)
 
-
-
-verkko.bf(0)
+graph.bf(0)
