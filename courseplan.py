2fd7fc312fed2752ca1ae97fa0f6640e631402b3
Toni Uimonen <tauimonen@gmail.com>
2021-06-04 14:16:30 +0300

    longest path algorithm

--- courseplan.py
+++ courseplan.py
@@ -5,7 +5,6 @@ computed as a hash table, where the keys are course names
 and the values are side lists.
 """
 
-
 class CoursePlan:
     def __init__(self):
         self.result = []
@@ -49,9 +48,9 @@ if __name__ == "__main__":
     c.add_course("Ohja")
     c.add_course("Tira")
     c.add_course("Jym")
-    c.add_requisite("Ohpe","Ohja")
-    c.add_requisite("Ohja","Tira")
-    c.add_requisite("Jym","Tira")
-    print(c.find()) # [Ohpe,Jym,Ohja,Tira]
-    c.add_requisite("Tira","Tira")
-    print(c.find()) # None
+    c.add_requisite("Ohpe", "Ohja")
+    c.add_requisite("Ohja", "Tira")
+    c.add_requisite("Jym", "Tira")
+    print(c.find()) # [Ohpe, Jym, Ohja, Tira]
+    c.add_requisite("Tira", "Tira")
+    print(c.find())     # None
