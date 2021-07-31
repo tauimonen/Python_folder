42b7ba649ef86fa9c628eb5777a8d4e89f776691
Toni Uimonen <tauimonen@gmail.com>
2021-07-31 18:43:00 +0300

    writing to file

--- venv/args.py
+++ venv/args.py
@@ -1,16 +1,16 @@
-# Just one way to catenate strings and center the text.
-# List comprehension could be easier.
+"""Just one way to catenate strings and center the text. List comprehension
+could be easier. Also writing a 'centered' file."""
 
-def centre_text(*args):
+def centered_text(*args, sep=" ", end="\n", file=None, flush=False):
     text = ""
     for arg in args:
         text += str(arg) + " "
     left_margin = (80 - len(text)) // 2
-    print(" " * left_margin, text)
+    print(" " * left_margin, text, end= end, file= file, flush=flush)
 
-
-# Calling the function
-centre_text("text, text and text")
-centre_text("text, text and text... and more text")
-fift = "last"
-centre_text("first", "second", 3, 4, fift)
+with open("centered", mode="w") as centered_file:
+    # Calling the function
+    centered_text("text, text and text", file=centered_file)
+    centered_text("text, text and text... and more text", file=centered_file)
+    fift = "last"
+    centered_text("first", "second", 3, 4, fift, sep=";", file=centered_file)
