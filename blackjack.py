2b6ddd5df49b5f0d4f697c1ccc80e976a2487113
tauimonen <tauimonen@gmail.com>
2021-09-15 21:46:51 +0300

    make frames and load cards

--- blackjack.py
+++ blackjack.py
@@ -7,17 +7,75 @@ except ImportError:     # Python 2
 
 mainWindow = tkinter.Tk()
 
+def load_images(card_images):
+    suits = ["heart", "club", "diamond", "spade"]
+    face_cards = ["jack", "queen", "king"]
+
+    if tkinter.TkVersion >= 8.6:
+        extension = "png"
+    else:
+        extension = "ppm"
+
+    # For each suit, retrieve the image for the card
+    for suit in suits:
+        for card in range(1, 11):
+            name = "cards/{}_{}.{}".format(str(card), suit, extension)
+            image = tkinter.PhotoImage(file=name)
+            card_images.append((card, image))
+
+        for card in face_cards:
+            name = "cards/{}_{}.{}".format(str(card), suit, extension)
+            image = tkinter.PhotoImage(file=name)
+            card_images.append((10, image))
+
+
 # Set up the screen and frames for the dealer and player
 mainWindow.title("Black Jack")
 mainWindow.geometry("640x480")
 
 result_text = tkinter.StringVar()
 result = tkinter.Label(mainWindow, textvariable=result_text)
-result.grid(row=0, colun=0, columnspan=3)
+result.grid(row=0, column=0, columnspan=3)
 
 card_frame = tkinter.Frame(mainWindow, relief="sunken", borderwidth= 1, background="green")
 card_frame.grid(row=1, column=0, sticky="ew", columnspan=3, rowspan=2)
 
 dealer_score_label = tkinter.IntVar()
 tkinter.Label(card_frame, text="Dealer", background="green", fg="white").grid(row=0, column=0)
-tkinter.Label(card_frame, text=dealer_score_label, background="green", fg="white").grid(row=1, column=0)
+tkinter.Label(card_frame, textvariable=dealer_score_label, background="green", fg="white").grid(row=1, column=0)
+
+# Embedded frame to hold the card image
+dealer_card_frame = tkinter.Frame(card_frame, background="green")
+dealer_card_frame.grid(row=0, column=1, sticky="ew", rowspan=2)
+
+player_score_label = tkinter.IntVar()
+tkinter.Label(card_frame, text="Player", background="green", fg="white").grid(row=2, column=0)
+tkinter.Label(card_frame, textvariable=player_score_label, background="green", fg="white").grid(row=3, column=0)
+
+# Embedded frame to hold the card image
+player_card_frame = tkinter.Frame(card_frame, background="green")
+player_card_frame.grid(row=2, column=1, sticky="ew", rowspan=2)
+
+button_frame = tkinter.Frame(mainWindow)
+button_frame.grid(row=3, column=0, columnspan=3, sticky="w")
+
+dealer_button = tkinter.Button(button_frame, text="Dealer")
+dealer_button.grid(row=0, column=0)
+
+player_button = tkinter.Button(button_frame, text="Player")
+player_button.grid(row=0, column=1)
+
+# load cards
+cards = []
+load_images(cards)
+print(cards)
+
+# Create a new deck of cards and shuffle them
+deck = list(cards)
+random.shuffle(deck)
+
+# Create the list to store the dealer's and player hands
+dealer_hand = []
+player_hand = []
+
+mainWindow.mainloop()
