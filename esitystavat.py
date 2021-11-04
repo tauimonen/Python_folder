ef8e52fa2a18c8862e2e6bc9e64ba239568c8a0f
tauimonen <tauimonen@gmail.com>
2021-11-04 22:24:56 +0200

    Algorithm timing

--- esitystavat.py
+++ esitystavat.py
@@ -2,7 +2,7 @@ def count(n, start=1):
     if n == 0:
         return 1
     ways = 0
-    for i in range(start,n+1):
+    for i in range(start, n+1):
         print(ways)
         ways += count(n-i, i)
     return ways
