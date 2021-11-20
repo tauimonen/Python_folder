c2e3622c96be26f06f6bb02d8fad9e4cbf58cc8f
tauimonen <tauimonen@gmail.com>
2021-11-20 22:54:55 +0200

    finding path

--- labyrinth.py
+++ labyrinth.py
@@ -1,17 +1,38 @@
+from collections import deque
+
+
+def find_path(graph, start, goal):
+    queue = deque([("", start)])
+    visited = set()
+
+    while queue:
+        path, current = queue.popleft()
+        if current == goal:
+            return path
+        if current in visited:
+            continue
+        visited.add(current)
+        for direction, neighbour in graph[current]:
+            queue.append((path + direction, neighbour))
+    return "NO WAY!"
+
+
 def count(r):
-    grid_height = len(r)
-    grid_width = len(r[0])
+    y = len(r)
+    x = len(r[0])
+
+    A, B = find_start_and_goal_coordinates(r)
+    find_path(r, A, B)
+
 
 def find_start_and_goal_coordinates(g):
     n = len(g)
-    start_coordinates = []
-    goal_coordinates = []
     for i in range(n):
         for j in range(0, len(g[0])):
             if g[i][j] == "A":
-                start_coordinates.append(1)
+                start_coordinates = (i, j)
             elif g[i][j] == "B":
-                goal_coordinates .append(0)
+                goal_coordinates = (i, j)
 
     return start_coordinates, goal_coordinates
 
