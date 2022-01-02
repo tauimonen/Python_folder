94a09e42c10b05fbaa0a35d111bf9867cad5df72
tauimonen <tauimonen@gmail.com>
2022-01-02 21:06:05 +0200

    recursive factorial working

--- factorial.py
+++ factorial.py
@@ -7,10 +7,22 @@ def fact(n):
     result = 1
     if n > 1:
         for f in range(2, n+1):
-            result += f
+            result *= f
     return result
 
 
+def factorial(n):
+    """
+    Calculate n! recursively
+    :param n: int
+    :return: int
+    """
+    if n <= 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+
 for i in range(130):
-    print(i, fact(i))
+    print(i, factorial(i))
 
